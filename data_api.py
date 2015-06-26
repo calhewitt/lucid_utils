4ff0cc61d1d279d3740bdb1347cb92833a66e9f5
Cal Hewitt <calhewitt@outlook.com>
2015-06-26 20:38:59 +0100

    Update data API

--- data_api.py
+++ data_api.py
@@ -1,111 +1,56 @@
-# An API for grabbing LUCID data files from the starserver
+# A wrapper for easily accessing the LUCID data API and fetching data in an easily processed format
 
+import urllib2 as urllib
 import numpy as np
-import urllib
 import json
-from datetime import date, datetime, timedelta
 
-class APIFrame:
+BASE_PATH = "http://starserver.thelangton.org.uk/lucid-data-browser/api/"
+
+def get_data_files(run = None):
+	stream = urllib.urlopen(BASE_PATH + "get/data_files")
+	data_files = json.loads(stream.read())
+	if run:
+		filtered_data_files = []
+		for data_file in data_files:
+			if data_file['run'] == run:
+				filtered_data_files.append(data_file)
+		return filtered_data_files
+	return data_files
 	
-	def __init__(self, json_str):
-		
-		self.json_str = json_str
-		self.channel = [json_str["channel"]]
-		self.config = json_str["config"]
-		self.daylight = json_str["daylight"]
-		self.latitude = json_str["latitude"]
-		self.longitude = json_str["longitude"]
-		self.run = json_str["run"]
-		self.sun_alt = json_str["sun_alt"]
-		self.timestamp = json_str["timestamp"]
-		self.url = {json_str["channel"]: json_str["url"]}
-		
-		self.channels = {}
-		
-	def grab(self):
-		for channel_id in self.url.keys():	
-			frame_txt = urllib.urlopen(self.url[channel_id])
-				
-			if frame_txt.getcode() == 200:
-				frame_txt = frame_txt.readlines()
+def get_runs():
+	# Get a list of available data files and extract runs from this
+	stream = urllib.urlopen(BASE_PATH + "get/data_files")
+	data_files = json.loads(stream.read())
+	runs = []
+	for data_file in data_files:
+		if not data_file['run'] in runs:
+			runs.append(data_file['run'])
+	return runs
+
+class Frame:
+	pass
+
+def get_frames(run, file_id):
+	stream = urllib.urlopen(BASE_PATH + "get/frames?data_file=" + str(file_id) + "&run=" + run)
+	frames = json.loads(stream.read())
+	updated_frames = []
+	for frame in frames:
+		frame_obj = Frame()
+		frame_obj.__dict__ = frame
+		frame = frame_obj
+		new_channels = []
+		for channel_id in range(5):
+			channel = np.zeros((256, 256))
+			if str(channel_id) in frame.channels.keys():
+				for line in frame.channels[str(channel_id)].split("\n")[:-1]: # Last line is blank
+					vals = line.split("\t")
+					x = int(float(vals[0].strip()))
+					y = int(float(vals[1].strip()))
+					c = int(float(vals[2].strip()))
+					channel[x][y] = c
+				new_channels.append(channel)
 			else:
-				raise Exception("That data file could not be found")
-			
-			frame = np.zeros((256, 256))
-			for line in frame_txt:
-				vals = line.split("\t")
-				x = int(float(vals[0].strip()))
-				y = int(float(vals[1].strip()))
-				c = int(float(vals[2].strip()))
-		
-				frame[x][y] = c
-				
-			self.channels[channel_id] = frame
-		return self.channels
-		
-	
-	def get_xyc(self):
-		try:
-			frame_txt = urllib.urlopen(self.url)
-		except:
-		
-			raise Exception("That data file could not be found")
-			
-		if frame_txt.getcode() == 200:
-			frame_txt = frame_txt.read()
-		else:
-			raise Exception("That data file could not be found")
-	
-		return frame_txt
-		
-	def merge(self, other_frame):
-		self.channel.append(other_frame["channel"])
-		self.url[other_frame["channel"]] = other_frame["url"]
-		
-
-def get_run(timestamp):
-	date_obj = datetime.fromtimestamp(timestamp).date()
-	diff = date_obj - date(2014, 12, 19)
-	diff = diff.days % 8
-	new_date = date_obj - timedelta(days=diff)
-	#print new_date	
-	return new_date.strftime("%Y-%m-%d")
-
-def get_frames_by_run(run_name):
-	
-	# construct the frame report URL and try to query it
-	report_url = "http://starserver.thelangton.org.uk/lucid-data-browser/frame-reports/" + run_name + "-frame-report.txt"
-	try:
-		frames = urllib.urlopen(report_url).read()
-		frames = json.loads(frames)
-
-	except:
-		raise Exception("That run could not be found")
-		
-	new_frames = []
-	current_frame = None
-	current_timestamp = 0
-	
-	for frame in frames:
-		if frame["timestamp"] == current_timestamp:
-			current_frame.merge(frame)
-		else:
-			if current_frame != None:
-				new_frames.append(current_frame)
-			current_frame = APIFrame(frame)
-			current_timestamp = current_frame.timestamp
-	new_frames.append(current_frame)
-			
-	return new_frames
-	
-def get_frame_by_timestamp(timestamp):
-	# A bit hacky I know,
-	# TODO built better support for things like this on the server side
-	frames = get_frames_by_run(get_run(timestamp))
-
-	for frame in frames:
-		if frame.timestamp == timestamp:
-			return frame
-
-	raise Exception("A frame could not be found with that timestamp")
-	
+				new_channels.append(None)
+		frame.channels = new_channels
+		updated_frames.append(frame)
+	return updated_frames
