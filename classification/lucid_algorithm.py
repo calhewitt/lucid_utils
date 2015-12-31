ec45c655fc8dccf3862fbe671b132b04668b449c
Cal Hewitt <calhewitt@outlook.com>
2015-12-31 18:30:08 +0000

    Add lucid_algorithm

--- classification/lucid_algorithm.py
+++ classification/lucid_algorithm.py
@@ -1,4 +1,3 @@
-# IMPORTANT: Very much in beta, do not use yet!!
 # AUTHOR: Cal Hewitt
 # An alternative analysis strategy based on the same metrics (calculated in common.py) as earlier algorithms
 # A 'mode' can be specified as an argument to the classify function - this can either be
@@ -8,7 +7,6 @@
 # Generally, the algorithm will classify 'agressively', ie. never returning a 'not sure' verdict - this is useful
 # for generating counts for mapping with LUCID data.
 # TODO develop separate modes for lower 'agression' settings
-# Sorry about snarky commenting. Will be removed soon.
 
 import common
 import end_detection
@@ -17,49 +15,38 @@ class Blob(common.Blob):
     def classify(self, mode):
         if not mode in ["MODE_LUCID", "MODE_CERNATSCHOOL"]:
             raise Exception("Invalid mode name, cannot continue")
-        # Start by getting gamma and alpha possibilities out of the way - they're eeeasy
+        # Start by eliminating a few trivial cases - if a blob has fewer than 4 pixels, then it must be a gamma (or there is no way of telling otherwise)
         if self.num_pixels < 4:
             return 'gamma'
+        # Large clusters with such a high density, or smaller ones with even higher densities are always alpha
         if (self.density > 0.75 and self.num_pixels > 7) or (self.radius < 9 and self.num_pixels > 90):
             return 'alpha'
-        # Must be beta, proton, muon or other.
-        # We've narrowed it down to one of the long, straight ones - result, eh?
-        # Aren't we clever?
-        # Right, let's get rid of small betas, these can't really be anything else
+        # By this stage, must be beta, proton, muon or other.
+        # Very small clusters will always be electron hits
         if self.num_pixels < 6 or self.radius < 2:
             return 'beta'
         # Whoop whoop!
-        # Right, let's now move on to the really straight long ones.
-        # They're either going to be protons or muons, and which of the two can be worked out from their length
-        # Anyway, enough talk for now. Let's do some CODING!!!!!
         if (self.squiggliness / self.radius) < 0.1 and self.radius > 20 and mode == "MODE_LUCID":
-            # Well, well, well then.
-            # The REALLY BIG ones are probably muons
-            # So
+            # Straight clusters over a certain radius will always be caused by a muon
             return 'muon'
             # Knock Knock
             # Who's there?
             # Interrupting physicist.
             # Interrupting physicist wh--
             # MUUUUUUUUON
-        # Otherwise, a kind of protonic designation seems in order
-        # The big ones only need be long and straigt...
+        # Otherwise, if the cluster is nearly completly straight it is probably a proton
         if self.radius > 7 and self.squiggliness < 0.4 and mode == "MODE_LUCID":
             return 'proton'
-        # But for smaller ones they also need to be 'chunky' otherwise they're probably electrons!!!!! OH NOES!!
+        # Shorter proton tracks can be distinguished from straight betas on their 'chunkiness'; the average width of the track
         if self.num_pixels / (2*self.radius) > 1.5 and self.squiggliness < 0.7 and mode == "MODE_LUCID":
             return 'proton'
-            # Yay
-        # Okay. The situation is looking generally pretty good, though the greatest trouble may still be ahead...
-        # What remains will either be
-        # (a) A standard beta
-        # (b) Crossed tracks / an interaction / or a particle shower
-        # Let's first get the possiblity of a small simple beta out of the way...
         if self.radius < 10 or mode == "MODE_CERNATSCHOOL":
-            # No real way to work more out about these
+            # If classifying aggressively, the only remaining possibility is a beta particle
             return 'beta'
-        # TODO an end detection check here - algorithm too slow at the moment for general use, so cheating :)
+        # TODO an end detection check here - algorithm too slow at the moment for general use, so cheating
+        # Want to be able to distinguish boring betas from interesting events or interactions
         return 'beta'
+        # More coming soon!
 
 
 def classify(blob, mode="MODE_LUCID"):
