c6627cccad6209f107cd8f718a91724adeeabd8b
willfurnell <willfurnell@users.noreply.github.com>
2016-03-12 21:43:34 +0000

    Python 3 requires spaces OR tabs indentation, also sorts tuple ref

--- prediction/__init__.py
+++ prediction/__init__.py
@@ -9,18 +9,19 @@ import geopy.distance
 import os
 
 # Predicts the (electon, proton) count per frame per second under NORMAL conditions in the given location
-def predict_background((lookup_lat, lookup_lng)):
+def predict_background(lookup_lat_and_lng):
+    (lookup_lat, lookup_lng) = lookup_lat_and_lng
     predicted_counts = {}
     for t in ["beta", "proton"]:
         counts = []
         data = open(os.path.dirname(os.path.abspath(__file__)) + "/" + t + "-calgorithm-counts.csv").readlines()
 
         for datum in data:
-        	datum = datum[:-1]  # Trim off newline char
-        	if datum:
-        		fields = datum.split(",")
-        		timestamp, latitude, longitude, num_frames, beta = fields
-        		latitude, longitude, beta, num_frames = float(latitude), float(longitude), int(beta), float(int(num_frames))
+            datum = datum[:-1]  # Trim off newline char
+            if datum:
+                fields = datum.split(",")
+                timestamp, latitude, longitude, num_frames, beta = fields
+                latitude, longitude, beta, num_frames = float(latitude), float(longitude), int(beta), float(int(num_frames))
                 counts.append((latitude, longitude, beta/num_frames))
 
         counts_sorted_distance = sorted(counts, key=lambda count:geopy.distance.great_circle((lookup_lat, lookup_lng), (count[0], count[1])))
