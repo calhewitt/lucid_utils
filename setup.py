42cd880aba5b6eb7dc04457a62f614f87d96376c
Cal Hewitt <calhewitt@outlook.com>
2016-08-16 17:34:16 +0100

    Set up setuputils with pip

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -11,7 +11,7 @@ setup(
     author_email = "hello@calhewitt.xyz",
     description = ("A library for analysing LUCID data"),
     license = "MIT",
-    packages=['lucid_utils'],
+    packages=find_packages(),
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
