6f93732b464fd4a93fda9372c5b49a1e790e6af4
Cal Hewitt <calhewitt@outlook.com>
2016-08-16 17:03:01 +0100

    Move to setuputils

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "lucid_utils",
+    version = "1.0",
+    author = "Cal Hewitt",
+    author_email = "hello@calhewitt.xyz",
+    description = ("A library for analysing LUCID data"),
+    license = "MIT",
+    packages=['lucid_utils'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
